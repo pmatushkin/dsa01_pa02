3a6b7044f8ffa3fcbbec2255e7c0803957c7f196
pmatushkin <pmatushkin@gmail.com>
2016-06-29 16:16:06 -0400

    feat: Pass Problem 1: Changing Money

--- src/change/Change.java
+++ src/change/Change.java
@@ -1,9 +1,27 @@
 import java.util.Scanner;
 
 public class Change {
+
     private static int getChange(int n) {
         //write your code here
-        return n;
+        final int c1 = 1;
+        final int c5 = 5;
+        final int c10 = 10;
+
+        int c = 0;
+        while (n != 0) {
+            if (n >= c10) {
+                n = n - c10;
+            } else if (n >= c5) {
+                n = n - c5;
+            } else {
+                n = n - c1;
+            }
+
+            c++;
+        }
+
+        return c;
     }
 
     public static void main(String[] args) {
